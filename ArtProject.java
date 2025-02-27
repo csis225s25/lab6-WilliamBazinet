711874ad9fd8cff2888187f7b62524cd5335c57d
Xhorxhi Olldashi <x19olld@siena.edu>
2025-02-27 12:52:34 -0500

    created the burj khalifa

--- ArtProject.java
+++ ArtProject.java
@@ -11,7 +11,41 @@ class GPan extends JPanel{
     @Override 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        g.drawArc(100, 42, 100, 100000, 84, 12);
+
+        /* setting background to blue */
+
+        /* fill blue */
+        g.setColor(Color.BLUE);
+        g.fillRect(0, 0, getWidth(), getHeight()/2);
+
+        /* grab gray crayon */
+        g.setColor(Color.GRAY);
+
+        /* the burj khalifa tower */
+        g.drawArc(600, 300, 100, 100000, 84, 12);
+
+        /* grab gray crayon */
+        g.setColor(Color.GRAY);
+
+        /* some buildings to place around the tower */
+        g.drawRect(570, 555, 10, 30);
+        g.drawRect(550, 554, 10, 33);
+        g.drawRect(515, 545, 10, 30);
+        g.drawRect(500, 550, 10, 35);
+        
+
+        /* it's like picking up a different crayon, yellow */
+        g.setColor(Color.YELLOW);
+
+        /* the sun */
+        g.fillOval(900, 42, 100, 100);
+
+        /* grab white crayon */
+        g.setColor(Color.WHITE);
+
+
+        /* cloud */
+        g.drawRoundRect(500, 400, 60, 30, 50000, 5);
     }
 }
 public class ArtProject implements Runnable {
